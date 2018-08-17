c893f5671e87a9c83a5b436edb87e48b94d20520
Gavin Moore <gavizoid16@gmail.com>
2018-08-17 03:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === 'hello') {
-    console.log("I can't hear you!")
+    return ("I can't hear you!")
   } else if (string === 'HELLO') {
-    console.log("YES INDEED!")
+    return ("YES INDEED!")
   } else {
     return ("I love you, too.")}
 }
